187e5d3ef8be4c3cd855a5171efa1390f6813915
Smr021 <161032439+Smr021@users.noreply.github.com>
2024-03-23 09:29:32 -0300

    Update SexoM.ou.F.c

--- SexoMouF.c
+++ SexoMouF.c
@@ -15,37 +15,38 @@ int main(){
 		
 	
 	printf("=== Peso Ideal === \n");
-	printf("Digite seu peso: \n");
-	scanf("%f",&peso);
-	
-	printf("Digite sua altura: ");
-	scanf("%f",&altura);
+	/*printf("Digite seu peso: \n");
+	scanf("%f",&peso);*/
 	
 	printf("Digite seu sexo [M | F]: ");
 	scanf("%c",&identificador);
 	
+	printf("Digite sua altura: ");
+	scanf("%f",&altura);
+	
+
 		
-	system("cls || clear ");
+	//system("cls || clear ");
 	
 	identificador = toupper(identificador);
 		
 	switch (identificador){
 		case 'M':
-		pesoIdeal = (72.7 * altura) - 58;
-		printf("Peso: %f.",peso);
-		printf("Peso ideal: %f.",pesoIdeal);
+		pesoIdeal = (72.7 * altura) - 58.00;
+	//	printf("Peso: %.2f.",peso);
+		printf("Peso ideal: %.2f.",pesoIdeal);
 			
 		break;
 					
 		case 'F': 
 		pesoIdeal = (62.1 * altura) - 44.7;
-		printf("Peso: %f.",peso);
-		printf("Peso ideal: %f.",pesoIdeal);
+	//	printf("Peso: %.2f.",peso);
+		printf("Peso ideal: %.2f.",pesoIdeal);
 			
 		break;
 			
 		default:
-		printf("Opção invalida, digite M ou F");
+		printf("OpÃ§Ã£o invalida, digite M ou F \n");
 		break;
 				
 	}
